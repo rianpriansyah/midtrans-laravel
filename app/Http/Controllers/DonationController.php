86a8100e6b1db08b645a5451a3d86a75bb3edc23
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-27 10:18:20 +0700

    membuat fungsi store payment

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -3,10 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Donation;
 
 class DonationController extends Controller
 {
     public function index() {
         return view('donation');
+    }
+
+    public function store(Request $request) {
+        \DB::transaction(function () use ($request) {
+            $donation = Donation::create([
+                'donor_name' => $request->donor_name,
+                'donor_email' => $request->donor_email,
+                'donation_type' => $request->donation_type,
+                'amount' => floatval($request->amount),
+                'note' => $request->note
+            ]);
+
+            $payload = [
+                'transaction_details' => [
+                    'order_id' => 'SANDBOX-' . uniqid(),
+                    'gross_amount' => $donation->amount,
+                ],
+                'customer_details' => [
+                    'first_name' => $donation->name,
+                    'first_email' => $donation->email,
+                ],
+                'item_details' => [
+                    [
+                        'id' => $donation->donation_type,
+                        'price' => $donation->amount,
+                        'quantity' => 1,
+                        'name' => ucwords(str_replace('_', ' ', $donation->donation_type))
+                    ]
+                ]
+            ];
+
+            $snapToken = \Midtrans\Snap::getSnapToken($payload);
+            $donation->snap_token = $snapToken;
+            $donation->save();
+
+            $this->response['snap_token'] = $snapToken;
+        });
+
+        return response()->json($this->response);
     }
 }
